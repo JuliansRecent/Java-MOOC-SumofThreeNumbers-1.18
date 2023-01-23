da32f5f0900881ec1ef32f09887842959430cb5a
JuliansRecent <juliansrecent@gmail.com>
2023-01-23 15:48:40 -0800

    Module complete. Prompt the user for three different numbers, then calculate the sum and print it to the console.

--- src/main/java/SumOfThreeNumbers.java
+++ src/main/java/SumOfThreeNumbers.java
@@ -7,6 +7,16 @@ public class SumOfThreeNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        // Proomt the user for three numbers and store them in variables
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int thirdNumber = Integer.valueOf(scanner.nextLine());
+        // Calculate the sum of the three numbers
+        int sum = firstNumber + secondNumber + thirdNumber;
+        // Output the sum of the three numbers
+        System.out.println("The sum of the numbers is " + sum);
     }
 }
